4db3d032506558fab667fca04676870b9c1da1e8
ali asadi <30907958+iamaliasadi@users.noreply.github.com>
2023-03-11 11:46:51 +0330

    added x_api_key

--- bitcoinpython/wallet.py
+++ bitcoinpython/wallet.py
@@ -248,7 +248,7 @@ class PrivateKey(BaseKey):
         return create_p2pkh_transaction(self, unspents, outputs, custom_pushdata=custom_pushdata)
 
     def send(self, outputs, fee=None, leftover=None, combine=True,
-             message=None, unspents=None):  # pragma: no cover
+             message=None, unspents=None,x_api_key=None):  # pragma: no cover
         """Creates a signed P2PKH transaction and attempts to broadcast it on
         the blockchain. This accepts the same arguments as
         :func:`~bitcoinpython.PrivateKey.create_transaction`.
@@ -287,7 +287,7 @@ class PrivateKey(BaseKey):
             outputs, fee=fee, leftover=leftover, combine=combine, message=message, unspents=unspents
         )
         
-        NetworkAPI.broadcast_tx(tx_hex)
+        NetworkAPI.broadcast_tx(tx_hex,x_api_key)
 
         return calc_txid(tx_hex)
 
